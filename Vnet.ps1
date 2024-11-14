443c787d9ae9e34db4cb40089b2ecfcee6dbeb07
Kedma Clement <kedmaclement@gmail.com>
2024-11-14 18:11:21 -0500

    updating vnet config using variables

--- Vnet.ps1
+++ Vnet.ps1
@@ -1,8 +1,16 @@
+# Define variables
+$resourceGroupName = "az-vm-webserver"
+$vnetName = "AZE-Vnet"
+$location = "EastUS"
+$addressPrefix = "10.0.0.0/16"
+$subnetName = "AZE-Subnet"
+$subnetPrefix = "10.0.0.0/24"
+
 # Create a virtual network
-$vnet = New-AzVirtualNetwork -ResourceGroupName "az-vm-webserver" -Name "AZE-Vnet" -Location "EastUS" -AddressPrefix "10.0.0.0/16"
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vnetName -Location $location -AddressPrefix $addressPrefix
 
 # Add a subnet to the virtual network
-Add-AzVirtualNetworkSubnetConfig -Name "AZE-Subnet" -AddressPrefix "10.0.0.0/24" -VirtualNetwork $vnet
+Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetPrefix -VirtualNetwork $vnet
 
 # Apply the subnet configuration
 Set-AzVirtualNetwork -VirtualNetwork $vnet
