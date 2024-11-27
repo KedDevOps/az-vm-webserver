874b93de875390d83a08b331f9b12f6e0bd033a7
Kedma Clement <kedmaclement@gmail.com>
2024-11-26 20:05:33 -0500

    updating resource group file

--- Resource-Group.ps1
+++ Resource-Group.ps1
@@ -2,23 +2,5 @@
 $resourceGroupName = "az-vm-webserver"
 $location = "East US"
 
-# Check if the Az module is installed, and install if needed
-if (-not (Get-Module -ListAvailable -Name Az)) {
-    Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
-}
-
-# Importing the Az module
-Import-Module Az.Accounts -Force
-
-# Prompt for Tenant ID and Subscription ID
-$tenantId = Read-Host -Prompt "Enter your Tenant ID"
-$subscriptionId = Read-Host -Prompt "Enter your Subscription ID"
-
-# Connecting to Azure Account
-Connect-AzAccount -TenantId $tenantId
-
-# Selecting the Azure Subscription
-Set-AzContext -SubscriptionId $subscriptionId
-
 # Creating a resource group in Azure East US region
 New-AzResourceGroup -Name $resourceGroupName -Location $location
