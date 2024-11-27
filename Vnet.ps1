3d010490d585bfb44098a88c0b157809436bd3d7
Kedma Clement <kedmaclement@gmail.com>
2024-11-26 20:40:49 -0500

    updating the Vnet file to use backtick

--- Vnet.ps1
+++ Vnet.ps1
@@ -7,10 +7,18 @@ $subnetName = "AZE-Subnet"
 $subnetPrefix = "10.0.0.0/24"
 
 # Create a virtual network
-$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vnetName -Location $location -AddressPrefix $addressPrefix
+$vnet = New-AzVirtualNetwork `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vnetName `
+    -Location $location `
+    -AddressPrefix $addressPrefix
 
 # Add a subnet to the virtual network
-Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetPrefix -VirtualNetwork $vnet
+Add-AzVirtualNetworkSubnetConfig `
+    -Name $subnetName `
+    -AddressPrefix $subnetPrefix `
+    -VirtualNetwork $vnet
 
 # Apply the subnet configuration
-Set-AzVirtualNetwork -VirtualNetwork $vnet
+Set-AzVirtualNetwork `
+    -VirtualNetwork $vnet
